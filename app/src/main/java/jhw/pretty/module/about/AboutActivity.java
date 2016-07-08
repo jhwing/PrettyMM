7df559b7acf5f67c1374715691de04d06a299860
jihongwen <jhwing@163.com>
2016-07-08 19:09:56 +0800

    update

---
+++ app/src/main/java/jhw/pretty/module/about/AboutActivity.java
@@ -0,0 +1,35 @@
+package jhw.pretty.module.about;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import jhw.pretty.R;
+
+/**
+ * Created by jihongwen on 16/7/8.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/jhw/pretty/module/main/MainActivity.java
+++ app/src/main/java/jhw/pretty/module/main/MainActivity.java
@@ -1,11 +1,14 @@
 package jhw.pretty.module.main;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.List;
 
@@ -14,6 +17,7 @@ import jhw.pretty.base.activity.BaseAppCompatActivity;
 import jhw.pretty.bean.RespData;
 import jhw.pretty.common.loadmore.FootView;
 import jhw.pretty.common.loadmore.LoadMoreUtil;
+import jhw.pretty.module.about.AboutActivity;
 import jhw.pretty.module.main.adapter.MainListAdapter;
 
 public class MainActivity extends BaseAppCompatActivity implements MainContract.IMainView {
@@ -39,6 +43,7 @@ public class MainActivity extends BaseAppCompatActivity implements MainContract.
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -62,7 +67,21 @@ public class MainActivity extends BaseAppCompatActivity implements MainContract.
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                startActivity(new Intent(this, AboutActivity.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onDestroy() {
